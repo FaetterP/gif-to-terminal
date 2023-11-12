6daa7b51fdb44519b34541ead1897b2d9099cbe5
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-11-12 14:36:11 +0300

    Сделан сервер, работающий с потоками
    Это позволяет не закрывать соединение и отправлять данные по таймеру.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log(123);
+import express from "express";
+import { Readable } from "stream";
+const app = express();
+
+app.get("/", function (req, res) {
+  const stream = new Readable();
+  stream._read = function noop() {};
+  stream.pipe(res);
+
+  return setInterval(() => {
+    stream.push(`${new Date()}`);
+  }, 1000);
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
